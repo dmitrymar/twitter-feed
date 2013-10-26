befd6e9e4b87d25cb32d701945238c0ecfa1a17e
oleg <dmitrymar@yahoo.com>
2013-10-26 11:04:11 -0400

    latest

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert('hey');
+
+
+(function ($) {
+
+    var jqxhr = $.getJSON( "feed.json", function(data) {
+        $("#test").append( "<li>" + data.statuses[0].user.profile_image_url + data.statuses[0].user.screen_name + "<br>" + data.statuses[0].text + "</li>" );
+    })
+
+/*        .fail(function() { console.log( "error" ); })
+        .always(function() { console.log( "complete" ); });*/
+
+// perform other work here ...
+
+// Set another completion function for the request above
+    /*jqxhr.complete(function() { console.log( "second complete" ); });*/
+
+})(jQuery);
